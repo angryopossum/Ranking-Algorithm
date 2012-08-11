dbd72fbe853b0e40ec049dea67177f14816f1bf6
AngryOpossum <al.s@bigmir.net>
2012-08-11 11:01:51 +0300

    Create command-line stucture in Ranking

--- bin/ranking.rb
+++ bin/ranking.rb
@@ -1,10 +1,43 @@
 #! /usr/bin/env ruby
 
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
+
 require '../lib/color/color_monkey_patch'
 
 
-p Dir.pwd
+def parse_options
+  options = {}
+  case ARGV[1]
+  when "-e"
+    options[:e] = ARGV[2]
+  when "-d"
+    options[:d] = ARGV[2]
+  end
+  return options
+end
 
-print "This is text\n".red
-print "This is text\n".pink
-print "This is text\n".green
+
+case ARGV[0]
+when "-c"
+  puts "start on #{parse_options()}".red
+when "-s"
+  puts "called stop".pink
+#when "restart"
+#  puts "called restart"
+else
+  puts <<-EOF
+Please provide command name
+
+Usage:
+  ranking -c
+  ranking -s
+  
+
+  options:
+
+    -e ENVIRONMENT. Default: development
+    -d DEAMON, true or false. Default: true
+
+EOF
+end
+
