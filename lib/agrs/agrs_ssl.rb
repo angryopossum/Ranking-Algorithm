854de2043506e1d92085e2624293ef8031e05044
AngryOpossum <al.s@bigmir.net>
2012-11-01 14:29:48 +0200

    Check agreement CA signature

--- lib/agrs/agrs_ssl.rb
+++ lib/agrs/agrs_ssl.rb
@@ -44,18 +44,21 @@ def check_agrs(f)
   @fs = root.elements['follower_score'].text
   @sig_i = root.elements['Signature'].elements['initiator_signature'].text
   @sig_f = root.elements['Signature'].elements['follower_signature'].text
+  @sig_ca = root.elements['Signature'].elements['ca_signature'].text
 
 
   @pkey_i="../keys/private/#{i}.key"
   @pub_key_i="../keys/public/#{i}_public.key"
   @pub_key_f="../keys/public/#{@f}_public.key"
+  @pub_key_ca="../keys/public/CA_public.key"
 
   @data = "#{@id}/#{@date}/#{@category}/#{i}/#{@f}/#{@is}/#{@fs}"
 
   @check_signature_initiator = check_signature(@pub_key_i,@data,Base64.decode64(@sig_i))
   @check_signature_follower = check_signature(@pub_key_f,@data,Base64.decode64(@sig_f))
+  @check_signature_ca = check_signature(@pub_key_ca,@data,Base64.decode64(@sig_ca))
 
-if @check_signature_initiator and @check_signature_follower then return true else return false end
+if @check_signature_initiator and @check_signature_follower and @check_signature_ca then return true else return false end
 
 end
 
