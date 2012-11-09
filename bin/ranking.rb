d101180b2fe6e0e85086f3e35fa8276518b7c00b
AngryOpossum <al.s@bigmir.net>
2012-11-09 14:27:13 +0200

    Comment check_category

--- bin/ranking.rb
+++ bin/ranking.rb
@@ -80,8 +80,8 @@ when "-c"
 
   @dir = "#{config['agreements_dir']}/*"
   @stat  = AgrsCheck.new
-  @cat =  @stat.get_category(@dir)
-  @stat.check_category(@cat,@dir)
+# @cat =  @stat.get_category(@dir)
+# @stat.check_category(@cat,@dir)
   
 
 
